dfacb54be785d7478f3629f7c4afe4fbc3b6e283
ABDULHAMEED IDRIS ADEDAMOLA <36794582+idris0001@users.noreply.github.com>
2024-09-04 22:34:56 +0900

    Add files via upload

---
+++ Fig2.3.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Multiple line of text with a single statement
+	// Print the result
+	fmt.Print("Welcome to ")
+	fmt.Printf("Go Programming! It seems it no difference between print and println here ")
+}
